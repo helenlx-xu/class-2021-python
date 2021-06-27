328a4e086e08c46f4f271ca8d354356302262585
Helen Xu <helen.lx.xu@icloud.com>
2021-06-27 22:14:22 +0800

    静夜思

---
+++ Jun2021/lesson7.0.py
@@ -0,0 +1,71 @@
+print("2021.6.27 homework")
+
+name = 'zhangsan'
+age = 19
+
+a1 = 100
+a_ = 200
+
+print(name)
+
+A = 100
+a = 200
+
+print(A)
+
+print(a)
+
+get_name_by_line = 'hello'
+
+getNameByLine = 'hello'
+
+GetNameByLine = 'hello'
+
+print(GetNameByLine)
+
+print(get_name_by_line)
+
+print(getNameByLine)
+print(getNameByLine)
+
+名字 ='zhangsan'
+
+print(名字)
+
+money = 28
+
+print(type(money))
+
+type(money)
+
+money = 280
+print(type(money))
+
+money = 28.9
+print(type(money))
+
+money = '1万'
+print(money)
+print(type(money))
+
+money = '10000'
+print(type(money))
+
+money = "10000"
+print(type(money))
+
+money ='''10000'''
+print(type(money))
+
+message = "she says:'great!'"
+print(message)
+
+shi = '''
+       静夜思
+            唐 李白
+      床前明月光，
+      疑是地上霜。
+      举头望明月，
+      低头思故乡。
+'''
+print(shi)
