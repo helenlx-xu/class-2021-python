1c4a44b200cda7dcc2af05ec077a9fb34721993c
Helen Xu <helen.lx.xu@icloud.com>
2021-06-27 21:03:45 +0800

    lesson6 workhome

--- Jun2021/README.md
+++ Jun2021/README.md
@@ -1,1 +1,6 @@
 # 2021.6 作业
+
+git add .
+git commit -m "{作业名称}"
+*Example: git commit -m "20200626-Lession1"
+git push -u origin main
---
+++ Jun2021/lesson6.py
@@ -0,0 +1,33 @@
+print("2021.6.27 homework")
+
+name = 'zhangsan'
+age = 19
+
+a1 = 100
+a_ = 200
+
+print(name)
+
+A = 100
+a = 200
+
+print(A)
+
+print(a)
+
+get_name_by_line = 'hello'
+
+getNameByLine = 'hello'
+
+GetNameByLine = 'hello'
+
+print(GetNameByLine)
+
+print(get_name_by_line)
+
+print(getNameByLine)
+print(getNameByLine)
+
+名字 ='zhangsan'
+
+print(名字)
