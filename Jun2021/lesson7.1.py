6b9954887e68d2d76cac8d7b36312068d6a74e86
Helen Xu <helen.lx.xu@icloud.com>
2021-06-28 19:18:14 +0800

    lesson7.1.py

---
+++ Jun2021/lesson7.1.py
@@ -0,0 +1,19 @@
+isLogin = True
+print(isLogin)
+
+isLogin = False
+print(isLogin)
+
+print(type(isLogin))
+
+userName=input('请输入用户名')
+
+print('哈哈哈哈')
+print(userName)
+print(type(userName))
+
+money=input('请输入交费金额')
+print(money)
+print(type(money))
+
+print(int(money)+1000)
---
+++ Jun2021/lesson8.0.py
@@ -0,0 +1,28 @@
+one = input('输入第一个数')
+two = input('输入第二个数')
+
+print(int(one)+int(two))
+
+print(float(one)+float(two))
+
+print(int(one)-int(two))
+
+print(float(one)-float(two))
+
+a = 9.49
+print(int(a))
+
+flag = True
+print(int(flag))
+print(float(flag))
+print(str(flag))
+
+flag = False
+print(int(flag))
+print(float(flag))
+print(str(flag))
+
+
+
+a = 5
+print(bool(a))
