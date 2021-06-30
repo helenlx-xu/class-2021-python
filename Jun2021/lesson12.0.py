7d1e3fe447bf9a08ae167138e534f5cb59ae97f7
Helen Xu <helen.lx.xu@icloud.com>
2021-06-30 19:07:14 +0800

    lesson.12.0.py

---
+++ Jun2021/lesson12.0.py
@@ -0,0 +1,23 @@
+a = 1
+b = 3
+print(b and a)
+
+c = 0
+print(c and a)
+
+print(a  > c and a < c)
+
+print(a == c and a < b)
+
+print('*' * 20)
+
+print(b or a)
+
+print(a > c or a < b)
+print(a == c or a < b)
+
+print('-*' * 20)
+
+flag = True
+
+print(not flag) 
---
+++ Jun2021/lesson13.0.py
@@ -0,0 +1,10 @@
+name = 'Alice'
+age = 18
+
+print('我喜欢听'+str(age)+'岁的'+name+'唱歌! ')
+
+money = 1000
+
+print('%d岁的%s一首歌挣了%f块钱' %(age,name,money))
+
+print('%s岁的%s一首歌挣了%s块钱' %(age,name,money))
