15f1e90f4935e86e858e7d8f7e94298792f91d03
Helen Xu <helen.lx.xu@icloud.com>
2021-07-01 18:48:57 +0800

    lesson.14.0.py

---
+++ Jun2021/lesson14.0.py
@@ -0,0 +1,32 @@
+n = 149
+result = bin(n)
+print(result)
+
+
+result= oct(n)
+print(result)
+
+
+n = 221
+result = hex(n)
+print(result)
+
+n = 0x558
+result = int(n)
+print(result)
+
+result = bin(n)
+print(result)
+
+x = 0b10101011000
+print(int(x))
+
+n1 = 0b1110
+n2 = 0b0010
+
+print(n1 & n2)
+print(5&9)
+
+print('*-'*20)
+print(n1|n2)
+print(5|9)
