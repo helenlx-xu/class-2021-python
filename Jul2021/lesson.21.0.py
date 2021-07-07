632d8e70e6f0af13a7892a0ce5b19a44512f121f
Helen Xu <helen.lx.xu@icloud.com>
2021-07-07 18:16:33 +0800

    lesson.21.0.py

---
+++ Jul2021/lesson.21.0.py
@@ -0,0 +1,41 @@
+a = 1
+b = 2
+c = a if a<b else b
+print(a, b, c)
+
+print('aaaa') if a<b else print('bbbbb')
+
+if a<b:
+    print('aaaa')
+else:
+    print('bbbb')
+
+if '':
+    print('11111')
+else:
+    print('00000')
+
+'''
+循环:
+场景:
+1.用户名和密码，反复输入
+2.计算1-100
+3.游戏，死了重生
+4. ....
+
+方式:
+1.while
+2.for
+
+while格式:
+
+whlie 条件：
+    要循环执行的代码
+
+布尔类型的条件
+'''
+
+n = 1
+while n <= 10:
+    print('------>n=%d' % n)
+    n += 1
