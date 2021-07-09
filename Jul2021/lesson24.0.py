a4c4ba7e2e4c37255bc80cd9da6588a1fce3da87
Helen Xu <helen.lx.xu@icloud.com>
2021-07-09 18:17:22 +0800

    lesson24.0.py

--- Jul2021/lesson24.0.py
+++ Jul2021/lesson24.0.py
@@ -5,11 +5,20 @@ m_count = 0
 while n <= 3:
     ran = random.randint(0,2)
     guess = int(input('请输入：剪刀(0) 石头(1) 布(2)\n'))
-    if guess == 0 and ran == 2 or guess == 1 and ran == 0 or guess == 2 and ran == 1:
-        print('~~~~ 我赢了! ~~~~')
+    if (guess == 0 and ran == 2) or (guess == 1 and ran == 0) or (guess == 2 and ran == 1):
+        print('~~~~ 本轮我赢了! ~~~~')
         p_count += 1
     elif ran == 0 and guess == 2 or ran == 1 and guess == 0 or ran == 2 and guess == 1:
-        print('~~~~ 机器赢了 ~~~~')
+        print('~~~~ 本轮机器赢了 ~~~~')
         m_count += 1
     else:
         print('本轮平局! ')
+
+        n += 1
+
+    if p_count > m_count:
+        print('最终我获胜了! ')
+    elif p_count < m_count:  
+        print('最终机器获胜了! ')
+    elif p_count == m_count:  
+        print('最终平局! ')
