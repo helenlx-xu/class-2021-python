906a0cda4ee8639d3017d4d5adf5d778b96d31cf
Helen Xu <helen.lx.xu@icloud.com>
2021-07-20 13:09:49 +0800

    lesson.34.0.py

---
+++ Jul2021/lesson.34.0.py
@@ -0,0 +1,21 @@
+flag = True
+while flag:
+    name = input('用户名/手机号码:')
+    if (name.islower() and  name[0].isalpha() and len(name) >= 6) or (name.isdigit() and   len(name) == 11):
+
+        while True:
+            password = input('密码:')
+
+            if len(password) == 6 and password.isdigit():
+
+                if (name == 'admin123' or name == '15811119999') and password == '200325':
+                    print('用户登录成功! ')
+                    flag = False
+                    break
+                else:
+                    print('用户名或者密码有误! ')
+                    break
+            else:
+                print('密码必须是6位数字')
+    else:
+        print('用户名或者手机号码格式错误! ')
