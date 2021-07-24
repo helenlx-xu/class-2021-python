5d339cad641d2864e83b10ea72fa0e9641d15e10
Helen Xu <helen.lx.xu@icloud.com>
2021-07-24 12:16:23 +0800

    lesson.48.0.py

---
+++ Jul2021/lesson.48.0.py
@@ -0,0 +1,23 @@
+import time
+all_role = []
+print('~~~~~~~~~~欢迎进入王者荣耀角色管理~~~~~~~~~~')
+while True:
+    choice = input('请选择功能:\n 1.添加角色 \n 2.删除角色 \n 3.修改角色 \n 4.查询角色 \n 6.退出系统 \n')
+
+    if choice == '1':
+        pass
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        pass
+    elif choice == '4':
+        pass
+    elif choice == '5':
+        pass
+    elif choice == '6':
+        print('正在退出王者荣耀管理系统~~~~')
+        time.sleep(3)
+        print('成功退出!')
+        break
+    else:
+        print('输入错误,重新选择')
