74c1fa3f5b79b7b338083de5384cdf6460a639ef
Helen Xu <helen.lx.xu@icloud.com>
2021-07-26 09:37:32 +0800

    lesson.53.54.0.py

--- Jul2021/lesson.53.54.0.py
+++ Jul2021/lesson.53.54.0.py
@@ -11,3 +11,12 @@ for i in book:
 
 book.setdefault('出版社','人民教育出版社')
 print(book)
+
+dict1 = {'a':10,'b':20}
+book.update(dict1)
+print(book)
+
+print(book)
+result = dict1.fromkeys(['a','b'],[1,20])
+
+print(result)
