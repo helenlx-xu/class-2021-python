a9285d706011a45956e173a8b1592ee9581cd7f9
Helen Xu <helen.lx.xu@icloud.com>
2021-07-27 10:55:33 +0800

    lesson.56.0.py

---
+++ Jul2021/lesson.56.0.py
@@ -0,0 +1,17 @@
+list1 = [1,3,6,8,9,9,1,2,3,9]
+set2 = set(list1)
+print(set2)
+
+set3 = set()
+print(type(set3))
+
+print(len(set3))
+
+set3.add('三体')
+print(set3)
+
+set3.add('红楼梦')
+print(set3)
+
+set3.add('盗墓笔记')
+print(set3)
