11b1c4b97c4d55aea35ad61764c81e656840185c
Helen Xu <helen.lx.xu@icloud.com>
2021-07-28 11:39:10 +0800

    lesson.60.0.py

---
+++ Jul2021/lesson.60.0.py
@@ -0,0 +1,17 @@
+list1 = [1,2,3,5,7,9,8]
+result = max(list1)
+print(result)
+
+result = min(list1)
+print(result)
+
+result = sum(list1)
+print(result)
+
+result = abs(-1)
+print(result)
+
+t1 = (45,12,78,90,10,55)
+
+result = sorted(t1,reverse=True)
+print(result)
