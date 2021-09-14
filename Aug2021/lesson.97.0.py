d6a8ecfeb86870ed65caec3fed1d840177e09794
Helen Xu <helen.lx.xu@icloud.com>
2021-09-14 12:44:29 +0800

    lesson.101.0.py

---
+++ Aug2021/lesson.97.0.py
@@ -0,0 +1,19 @@
+A = 30
+
+print(a,A)
+
+name01 = '张三'
+print(name01)
+
+name01 = '李四'
+print(name01)
+
+_name01 = '王五'
+print(name01)
+
+
+goods_num = 10
+goods_total = 289.9
+
+print('商品数量是:',goods_num)
+print('商品的总额是:',goods_total)
---
+++ Sep2021/lesson.101.0.py
@@ -0,0 +1,13 @@
+print(' hello\py\thon')
+
+print(' hello\python')
+
+print(' hello\py\\thon')
+
+print('hello')
+
+value = 'hello'
+print(value)
+
+value = 'python'
+print(value)
