4f6e2f6bffba9fd0d39910b6ed92506205ceee05
Helen Xu <helen.lx.xu@icloud.com>
2021-10-10 23:10:27 +0800

    lesson.102.0.py

--- Oct2021/lesson.102.0.py
+++ Oct2021/lesson.102.0.py
@@ -3,6 +3,6 @@ address = '北京市海淀区中关村智科技大厦4层'
 phone = '1585858888'
 num = 5
 
-print('订单的收件人是：’+person+'收货地址是:'+address+'联系放式:'+phone+',商品数量是:'+num+')
+print('订单的收件人是：'+person+'收货地址是:'+address+',联系方式:'+phone+',商品数量是:'+ num')
 
 print('订单的收件人是:%s,收货地址是:%s,联系方式:%s,商品数量是:%s' % (person,address,phone,num))
