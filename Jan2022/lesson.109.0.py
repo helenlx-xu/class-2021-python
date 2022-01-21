403c1759a2f098c2c10887256201cc3cc954de42
Helen Xu <helen.lx.xu@icloud.com>
2022-01-21 21:59:50 +0800

    lesson.109.0.py

---
+++ Jan2022/lesson.109.0.py
@@ -0,0 +1,39 @@
+num = 142857
+num *= 1
+print(num)
+
+a = 2
+b = 1
+result = a - b
+print(result)
+print('减法运算:',result)
+
+a = 2
+b = 2
+result = a*b
+print(result)
+a = 4
+b = 4
+result = a*b
+a = 8
+b = 8
+result = a*b
+print(result)
+a = 10
+b = 10
+result = a*b
+print(result)
+
+
+a = 9
+b = 2
+result =a // b
+print('除法运算:', result)
+
+result =a % b
+print('取余数运算:', result)
+
+b = 3
+result =a ** b
+print('乘法运算:', result)
+
