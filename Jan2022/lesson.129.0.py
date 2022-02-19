6758fc882100896f3a3dc49c6d8ce8a03c057e52
Helen Xu <helen.lx.xu@icloud.com>
2022-02-19 10:37:45 +0800

    lesson.129.0.py

---
+++ Jan2022/lesson.129.0.py
@@ -0,0 +1,32 @@
+filename = 'picture.png'
+
+print(filename[0])
+
+
+print(filename[0:7])
+print(filename[3:7])
+
+print(filename[3:])
+print(filename[:7])
+
+print(filename[8:-1])
+
+print(filename[:-2])
+
+print(filename[-1:])
+
+print(filename[-5:-1])
+
+print(filename[10:0])
+
+print(filename[::-1])
+
+str1 = 'abcdefg'
+
+print(str1[-1:-5:-1])
+
+print(str1[5:0:-1])
+
+print('---->',str1[5:0:1])
+
+print(str1[::-1])
