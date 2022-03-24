4ddff9b1d45ecc7c0b21b067f06589a98e7cefaa
Helen Xu <helen.lx.xu@icloud.com>
2022-03-24 22:44:06 +0800

    lesson.1111.0.py

--- Feb2022/lesson.1111.py
+++ Feb2022/lesson.1111.py
@@ -31,3 +31,93 @@ A = "Sally"
 
 print(a)
 print(A)
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+#开头不可用数字，中间不能有空格和 -
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+print('----------SECTION 1-------------------------')
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+print('----------SECTION 2-------------------------')
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+print('----------SECTION 3-------------------------')
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+print('----------SECTION 4-------------------------')
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+print('----------SECTION 5-------------------------')
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+print('----------SECTION 6-------------------------')
+
+x = "awesome"
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
+
+
