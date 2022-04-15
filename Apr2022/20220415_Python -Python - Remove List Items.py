2416756adcc9d753d1c62160f4f53994ed388ec7
Helen Xu <helen.lx.xu@icloud.com>
2022-04-15 18:23:01 +0800

    20220415_Python -Python - Loop Through a List.p

---
+++ Apr2022/20220415_Python -Python - Remove List Items.py
@@ -0,0 +1,20 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+# the pop() method removes the last item
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
---
+++ 机密文件.py/zhrsqxjuejuezi.py
@@ -0,0 +1,5 @@
+thislist = ["S", "Z", "B","H","Z","G","Y"]
+thislist.insert(7, "Y")
+thislist.insert(8, "D")
+thislist.insert(9, "S")
+print(thislist)
