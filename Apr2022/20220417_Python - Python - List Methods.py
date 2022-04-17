6fb5bf2e851a648de3b56e691d984a8576c4309d
Helen Xu <helen.lx.xu@icloud.com>
2022-04-17 15:04:04 +0800

    20220417_Python - Python - List Methods.py

---
+++ Apr2022/20220417_Python - Python - List Methods.py
@@ -0,0 +1,88 @@
+                                #Python List append() Method
+#Add an element to the fruits list:
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+print(fruits)
+
+a = ["apple", "banana", "cherry"]
+b = ["Ford", "BMW", "Volvo"]
+a.append(b)
+print(a)
+
+                                #Python List clear() Method
+fruits = ["apple", "banana", "cherry"]
+fruits.clear()
+print(fruits)
+
+                                #Python List copy() Method
+fruits = ["apple", "banana", "cherry"]
+x = fruits.copy()
+print(x)
+          
+                                #Python List count() Method
+fruits = ["apple", "banana", "cherry"]
+x = fruits.count("cherry")
+print(x)
+
+fruits = [1, 4, 2, 9, 7, 8, 9, 3, 1]
+x = fruits.count(9)
+print(x)
+
+                                #Python List count() Method
+fruits = ["apple", "banana", "cherry"]
+x = fruits.count("cherry")
+print(x)
+
+                                #Python List extend() Method
+fruits = ['apple', 'banana', 'cherry']
+cars = ['Ford', 'BMW', 'Volvo']
+fruits.extend(cars)
+print(fruits)
+
+fruits = ['apple', 'banana', 'cherry']
+points = (1, 4, 5, 9)
+fruits.extend(points)
+print(fruits)
+
+                                #Python List index() Method
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.index("cherry")
+print(x)
+
+fruits = [4, 55, 64, 32, 16, 32]
+x = fruits.index(32)
+print(x)
+
+                                #Python List insert() Method
+fruits = ['apple', 'banana', 'cherry']
+fruits.insert(1, "orange")
+print(fruits)
+
+                                #Python List pop() Method
+fruits = ['apple', 'banana', 'cherry']
+fruits.pop(1)
+print(fruits)
+
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.pop(1)
+print(x)
+
+                                #Python List remove() Method
+fruits = ['apple', 'banana', 'cherry']
+fruits.remove("banana")
+print(fruits)
+
+                                #Python List reverse() Method
+fruits = ['apple', 'banana', 'cherry']
+fruits.reverse()
+print(fruits)
+
+                                #Python List sort() Method
+cars = ['Ford', 'BMW', 'Volvo']
+cars.sort()
+print(cars)
+
+cars = ['Ford', 'BMW', 'Volvo']
+cars.sort(reverse=True)
+print(cars)
+
