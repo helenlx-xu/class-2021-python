e22d09d4488be50d39c72aace82cb5a846f45f85
Helen Xu <helen.lx.xu@icloud.com>
2022-04-28 22:00:04 +0800

    20220428_Python - Python - Set Methods.py

--- Apr2022/20220427_Python - Python - Loop Sets.py
+++ Apr2022/20220427_Python - Python - Loop Sets.py
@@ -1,21 +1,4 @@
 thisset = {"apple", "banana", "cherry"}
-thisset.remove("banana")
-print(thisset)
 
-thisset = {"apple", "banana", "cherry"}
-thisset.discard("banana")
-print(thisset)
-
-thisset = {"apple", "banana", "cherry"}
-x = thisset.pop()
-print(x)
-print(thisset)
-
-thisset = {"apple", "banana", "cherry"}
-thisset.clear()
-print(thisset)
-
-thisset = {"apple", "banana", "cherry"}
-del thisset
-print(thisset)
-
+for x in thisset:
+  print(x)
---
+++ Apr2022/20220428_Python - Python - Set Methods.py
@@ -0,0 +1,55 @@
+                                #Python Set add() Method
+fruits = {"apple", "banana", "cherry"}
+
+fruits.add("orange") 
+
+print(fruits)
+
+
+
+fruits = {"apple", "banana", "cherry"}
+
+fruits.add("apple")
+
+
+fruits = {"apple", "banana", "cherry"}
+
+fruits.add("apple")
+
+
+                                #Python Set clear() Method
+
+thisset = {"apple", "banana", "cherry"}
+
+thisset.clear()
+
+print(thisset)
+
+
+
+print(fruits)
+
+
+                                #Python Set copy() Method
+fruits = {"apple", "banana", "cherry"}
+
+x = fruits.copy()
+
+print(x)
+
+
+                                #Python Set difference() Method
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = x.difference(y) 
+
+print(z)
+
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+z = y.difference(x) 
+
+print(z)
