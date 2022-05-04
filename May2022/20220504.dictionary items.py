dd4d4e53818acda993cd278acdaf58dc1df12de6
Helen Xu <helen.lx.xu@icloud.com>
2022-05-04 22:21:08 +0800

    20220504.dictionary items.py

---
+++ May2022/20220504.dictionary items.py
@@ -0,0 +1,13 @@
+thisdict = {
+  "color": "white",
+  "length": 50,
+  "width": 30,
+  "height" : 170,
+  "shape" : "cuboid",
+  "year" : 2016,
+  "name" : "shelf",
+  "age" : 6,
+  "drawers" : "2",
+  "stickers" : True
+}
+print(thisdict["color"])
