35e984d553c4a1ca1c30016357ca4d36bb5ae1a6
Helen Xu <helen.lx.xu@icloud.com>
2022-05-10 13:11:34 +0800

    20220509_Python Arrays.py

---
+++ 20220510_ Python Arrays.py
@@ -0,0 +1,63 @@
+cars = ["Ford", "Volvo", "BMW"]
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+x = cars[0]
+print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars[0] = "Toyota"
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+x = len(cars)
+print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+for x in cars:
+  print(x)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.append("Honda")
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.pop(1)
+print(cars)
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.remove("Volvo")
+print(cars)
+
+#下面是自己写的代码
+fruits = ['apple','banana','peach']
+print(fruits)
+
+fruits = ['apple','banana','peach']
+x = fruits[2]
+print(x)
+
+
+fruits = ['apple','banana','peach']
+fruits[0] = 'orange'
+print(fruits)
+
+fruits = ['apple','banana','peach']
+x = len(cars)
+print(x)
+
+fruits = ['apple','banana','peach']
+for x in fruits:
+  print(x)
+
+fruits = ['apple','banana','peach']
+fruits.append("apple")
+print(fruits)
+
+fruits = ['apple','banana','peach']
+fruits.pop(1)
+print(fruits)
+
+fruits = ['apple','banana','peach']
+fruits.remove("banana")
+print(fruits)
