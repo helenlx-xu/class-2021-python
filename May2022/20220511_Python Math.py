9e6663b07fd1497861ff3a80e4f15537da0db3ed
Helen Xu <helen.lx.xu@icloud.com>
2022-05-11 21:30:56 +0800

    20220509_Python Math.py

---
+++ May2022/20220511_Python Math.py
@@ -0,0 +1,75 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+
+x = abs(-7.25)
+
+print(x)
+
+
+x = pow(4, 3)
+
+print(x)
+
+
+import math
+
+x = math.sqrt(64)
+
+print(x)
+
+
+
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+
+
+
+import math
+
+x = math.pi
+
+print(x)
+
+
+#下面是自己写的
+x = min(1,2,3)
+y = max(1,2,3)
+print(x)
+print(y)
+
+
+x = abs(-1)
+print(x)
+
+
+x = pow(2, 45)
+print(x)
+
+
+import math
+x = math.sqrt(4900)
+print(x)
+
+
+
+import math
+x = math.ceil(1.9)
+y = math.floor(2.1)
+print(x)
+print(y)
+
+
+
+import math
+x = math.pi
+print(x)
