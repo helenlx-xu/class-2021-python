db248447eae2380918c501f635c6e90088f6942d
Helen Xu <helen.lx.xu@icloud.com>
2022-05-13 23:18:04 +0800

    20220512_Python User Input.py

---
+++ May2022/20220513_Python User Input.py
@@ -0,0 +1,19 @@
+username = input("Enter username:")
+print("Username is: " + username)
+#这种代码是python3.6版本用的
+
+# username = raw_input("Enter username:")
+# print("Username is: " + username)
+#这种代码是python2.7版本用的
+
+
+
+
+
+# #下面是自己写的代码
+username = input("Enter username:")
+print("Username is: " + username)
+
+
+# username = raw_input("Enter username:")
+# print("Username is: " + username)
