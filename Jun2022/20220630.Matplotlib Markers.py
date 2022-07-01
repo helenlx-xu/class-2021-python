e59d289d1ab5a68a7c28d482fcfc99186754a428
Helen Xu <helen.lx.xu@icloud.com>
2022-07-01 21:16:32 +0800

    20220630.Matplotlib Markers.py

---
+++ Jun2022/20220630.Matplotlib Markers.py
@@ -0,0 +1,72 @@
+# #Three lines to make our compiler able to draw:
+# import sys
+# import matplotlib
+# matplotlib.use('Agg')
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 8, 1, 10])
+
+# plt.plot(ypoints, marker = 'o')
+# plt.show()
+
+# #Two  lines to make our compiler able to draw:
+# plt.savefig(sys.stdout.buffer)
+# sys.stdout.flush()
+
+
+
+# #Three lines to make our compiler able to draw:
+# import sys
+# import matplotlib
+# matplotlib.use('Agg')
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 8, 1, 10])
+
+# plt.plot(ypoints, 'o:r')
+# plt.show()
+
+# #Two  lines to make our compiler able to draw:
+# plt.savefig(sys.stdout.buffer)
+# sys.stdout.flush()
+
+
+# #自己写:
+# import sys
+# import matplotlib
+# matplotlib.use('Agg')
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([4, 5, 2, 10])
+
+# plt.plot(ypoints, marker = 'o')
+# plt.show()
+
+
+# plt.savefig(sys.stdout.buffer)
+# sys.stdout.flush()
+
+
+
+
+# import sys
+# import matplotlib
+# matplotlib.use('Agg')
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# ypoints = np.array([3, 4, 5, 10])
+
+# plt.plot(ypoints, 'o:r')
+# plt.show()
+
+# plt.savefig(sys.stdout.buffer)
+# sys.stdout.flush()
+print(1+2)
