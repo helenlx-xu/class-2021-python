e41bd730359ddf1ba3a9a1a57ecab6ffbf41ed01
Helen Xu <helen.lx.xu@icloud.com>
2022-07-08 18:10:12 +0800

    20220708.Machine Learning - Standard Deviation.py

---
+++ Jul2022/20220708.Machine Learning - Standard Deviation.py
@@ -0,0 +1,57 @@
+import numpy
+
+speed = [86,87,88,86,87,85,86]
+
+x = numpy.std(speed)#均方差
+
+print(x)
+
+
+
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.std(speed)
+
+print(x)
+
+
+
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.var(speed)
+
+print(x)
+
+
+
+import numpy
+
+speed = [32,111,138,28,59,77,97]
+
+x = numpy.std(speed)
+
+print(x)
+
+
+#自己写
+import numpy
+
+speed = [22,23,24,22,23]
+
+x = numpy.std(speed)
+
+print(x)
+
+
+
+import numpy
+
+speed = [1,2,3,4,3,1,1,2]
+
+x = numpy.std(speed)
+
+print(x)
