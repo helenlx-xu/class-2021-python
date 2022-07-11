afd4982451ad04fead50bab7e25b55063cdae5ab
Helen Xu <helen.lx.xu@icloud.com>
2022-07-11 22:44:43 +0800

    20220711,Machine Learning - Normal Data Distribution ❮ PreviousNext ❯

---
+++ Jul2022/20220711,Machine Learning - Normal Data Distribution ❮ PreviousNext ❯.py
@@ -0,0 +1,30 @@
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy
+import matplotlib.pyplot as plt
+
+x = numpy.random.normal(5.0, 1.0, 100000)
+
+plt.hist(x, 100)
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
+
+#自己写
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import numpy
+import matplotlib.pyplot as plt
+x = numpy.random.normal(3.0, 1.0, 100000)
+plt.hist(x, 100)
+plt.show()
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
