fb12b8da87459de22c20febd8f6a1c7d26747ada
Helen Xu <helen.lx.xu@icloud.com>
2022-07-17 22:15:29 +0800

    20220717.Machine Learning - Scale.py

---
+++ Jul2022/20220717.Machine Learning - Scale.py
@@ -0,0 +1,41 @@
+import pandas
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+scale = StandardScaler()
+
+df = pandas.read_csv("cars2.csv")
+
+X = df[['Weight', 'Volume']]
+
+scaledX = scale.fit_transform(X)
+
+print(scaledX)
+
+
+
+
+
+
+
+
+import pandas
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+scale = StandardScaler()
+
+df = pandas.read_csv("cars2.csv")
+
+X = df[['Weight', 'Volume']]
+y = df['CO2']
+
+scaledX = scale.fit_transform(X)
+
+regr = linear_model.LinearRegression()
+regr.fit(scaledX, y)
+
+scaled = scale.transform([[2300, 1.3]])
+
+predictedCO2 = regr.predict([scaled[0]])
+print(predictedCO2)
+
+#标准化 去均值，方差规模化
