d91d24e21bc0881e56f63c60327fdecfc2424a01
Helen Xu <helen.lx.xu@icloud.com>
2022-07-21 21:40:29 +0800

    20220719.20.21.Machine Learning - Train.py

--- Jul2022/20220719.20.21.Machine Learning - Train.py
+++ Jul2022/20220719.20.21.Machine Learning - Train.py
@@ -106,3 +106,18 @@ plt.savefig(sys.stdout.buffer)
 sys.stdout.flush()
 
 
+
+
+#自己
+import numpy
+from sklearn.metrics import r2_score
+numpy.random.seed(2)
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+train_x = x[:80]
+train_y = y[:80]
+test_x = x[80:]
+test_y = y[80:]
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+print(mymodel(5))
+
