91931cf0296c8c69393b7eab68a328a25d0fad23
Helen Xu <helen.lx.xu@icloud.com>
2022-08-10 22:05:00 +0800

    2022080809.Machine Learning - Decision Tree.py

--- Aug2022/2022080809.Machine Learning - Decision Tree.py
+++ Aug2022/2022080809.Machine Learning - Decision Tree.py
@@ -21,3 +21,18 @@ print(dtree.predict([[40, 10, 7, 1]]))
 print("[1] means 'GO'")
 print("[0] means 'NO'")
 
+
+
+
+import pandas
+from sklearn import tree
+import pydotplus
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+import matplotlib.image as pltimg
+
+df = pandas.read_csv("shows.csv")
+
+print(df)
+
+#计算出最合适的结果但有时并不准确
---
+++ Aug2022/20220810.Machine Learning - Hierarchical Clustering ❮ PreviousNext ❯.py
@@ -0,0 +1,48 @@
+#3分层聚类
+
+
+
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+plt.scatter(x, y)
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
+#1.在图库中绘制代表数据的点
+
+
+#Three lines to make our compiler able to draw:
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+x = [4, 5, 10, 4, 3, 11, 14 , 6, 10, 12]
+y = [21, 19, 24, 17, 16, 25, 24, 22, 21, 21]
+
+data = list(zip(x, y))
+
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+
+plt.show()
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+#再再几个点中看横轴的间隔离得最近的画线连接 第二近的再画一条更长的线连接,以此类推 类似于树状图的东西就做好了
