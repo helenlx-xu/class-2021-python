e9366d4d73780a0145e911bc9144333a218279b3
Helen Xu <helen.lx.xu@icloud.com>
2022-08-11 21:57:57 +0800

    2022081011.Machine Learning - Hierarchical Clustering ❮ PreviousNext ❯.py

--- Aug2022/2022081011.Machine Learning - Hierarchical Clustering ❮ PreviousNext ❯.py
+++ Aug2022/2022081011.Machine Learning - Hierarchical Clustering ❮ PreviousNext ❯.py
@@ -46,3 +46,34 @@ plt.show()
 plt.savefig(sys.stdout.buffer)
 sys.stdout.flush()
 #再再几个点中看横轴的间隔离得最近的画线连接 第二近的再画一条更长的线连接,以此类推 类似于树状图的东西就做好了
+
+
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import numpy as np
+import matplotlib.pyplot as plt
+x = [1,2,8,3,6,3,6]
+y = [1,6,3,8,7,4,9]
+plt.scatter(x, y)
+plt.show()
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
+
+
+
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
+x = [1,2,8,3,6,3,6]
+y = [1,6,3,8,7,4,9]
+data = list(zip(x, y))
+linkage_data = linkage(data, method='ward', metric='euclidean')
+dendrogram(linkage_data)
+plt.show()
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
