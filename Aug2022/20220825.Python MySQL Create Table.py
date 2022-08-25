d4d1bf52e4a1649c57f439d5f0c4eb1af06960d5
Helen Xu <helen.lx.xu@icloud.com>
2022-08-25 21:19:56 +0800

    20220825.Python MySQL Create Table.py

---
+++ Aug2022/20220825.Python MySQL Create Table.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myusername",
+  password="mypassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+
+#If this page is executed with no error, you have successfully created a table named "customers".
+
+
+
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="myusername",
+  password="mypassword",
+  database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW TABLES")
+
+for x in mycursor:
+     print(x)
+
